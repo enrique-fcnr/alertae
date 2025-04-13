17ee95fd095203ffc961fb1a763b53e1cf645dff
“KellyReisLee” <kristinaalvares28@gmail.com>
2025-04-13 02:05:50 +0100

    Changing basic style in dashboard page

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,28 +6,20 @@ function Sidebar() {
   return (
     <>
       <div id="sidebar">
-
         <div>
           <form id="search-form" role="search">
             <input
+              className='sidebar-input'
               id="q"
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
             />
-            <div
-              id="search-spinner"
-              aria-hidden
-              hidden={true}
-            />
-            <div
-              className="sr-only"
-              aria-live="polite"
-            ></div>
+
           </form>
           <form method="post">
-            <button type="submit">New</button>
+            <button className='btn-sidebar  ' type="submit">New</button>
           </form>
         </div>
         <nav>
