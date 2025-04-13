3b473ce883b51c82b91c9558d1094ced7233df34
“KellyReisLee” <kristinaalvares28@gmail.com>
2025-04-13 03:03:55 +0100

    Structuring login basic loginpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,8 @@
-import Footer from "./components/Footer/Footer.jsx"
-import Navbar from "./components/Navbar/Navbar.jsx"
-import Hero from "./components/Hero/Hero.jsx"
-import Funcionalidades from "./components/Funcionalidades/Funcionalidades.jsx"
-import Beneficios from "./components/Beneficios/Beneficios.jsx"
-import UsoApp from "./components/UsoApp/UsoApp.jsx"
-import InterfaceApp from "./components/InterfaceApp/InterfaceApp.jsx"
-import Planos from "./components/Planos/Planos.jsx"
-import DownloadApp from "./components/DownloadApp/DownloadApp.jsx"
-import Depoimentos from "./components/Depoimentos/Depoimentos.jsx"
-import Contato from "./components/Contato/Contato.jsx"
 
 function App() {
   return (
     <>
-      {/* <Navbar />
-      <Hero />
-      <Funcionalidades />
-      <Beneficios />
-      <UsoApp />
-      <InterfaceApp />
-      <Planos />
-      <DownloadApp />
-      <Depoimentos />
-      <Contato />
-      <Footer /> */}
+
     </>
   )
 }
