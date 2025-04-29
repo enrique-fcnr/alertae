5971c6af03028970ef97508d3c818982cbb0515c
“KellyReisLee” <kristinaalvares28@gmail.com>
2025-04-30 00:11:31 +0100

    modifying the LoadingSkeletonPrevisoes section

--- src/components/LoadingSkeletonPrevisoes/LoadingSkeletonPrevisoes.jsx
+++ src/components/LoadingSkeletonPrevisoes/LoadingSkeletonPrevisoes.jsx
@@ -4,12 +4,10 @@ import { Skeleton } from "@chakra-ui/react"
 
 function LoadingSkeletonPrevisoes() {
   return (
-    <div className="d-flex flex-column gap-4">
+    <div className="container d-flex flex-column gap-4 mt-5">
       <div className="d-grid gap-4">
         <Skeleton className="w-100 rounded" style={{ height: "200px" }} />
-        <div className="row g-4">
-          <Skeleton className="w-100 rounded" style={{ height: "400px" }} />
-        </div>
+        <Skeleton className="w-100 rounded" style={{ height: "400px" }} />
       </div>
     </div>
   )
