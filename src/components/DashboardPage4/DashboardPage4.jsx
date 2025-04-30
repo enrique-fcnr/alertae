112be617f8d1bfe96673f53cac5fe6aa54a2e672
enrique-fcnr <enriquefcnrgta@gmail.com>
2025-04-30 13:11:11 -0300

    Creation and styling DashboardPage4.css

--- src/components/DashboardPage4/DashboardPage4.jsx
+++ src/components/DashboardPage4/DashboardPage4.jsx
@@ -5,7 +5,8 @@ import HumidityChart from '../HumidityChart/HumidityChart';
 import HottestDaysList from '../HottestDaysList/HottestDaysList';
 import WindTrendLine from '../WindTrendLine/WindTrendLine';
 import TemperatureHeatmap from '../TemperatureHeatmap/TemperatureHeatmap';
-import { weatherData } from '../../../weatherData'
+import { weatherData } from '../../../weatherData';
+import './DashboardPage4.css'; 
 
 
 
@@ -16,24 +17,36 @@ const DashboardPage4 = () => {
       <div className="">
         <div className="row row-cols-1 row-cols-md-2  g-3">
           <div className="col-12 col-md-12">
-            <TemperatureChart key="temp-chart" />
+          <div className="dashboard-card">
+              <TemperatureChart key="temp-chart" />
+              </div>
           </div>
           <div className="col-12 col-md-6">
-            <RainVolumeChart key="rain-chart" data={weatherData} />
+          <div className="dashboard-card">
+              <RainVolumeChart key="rain-chart" data={weatherData} />
+              </div>
           </div>
           <div className="col-12 col-md-6">
-            <HumidityChart key="humidity-chart" data={weatherData} />
+          <div className="dashboard-card">
+              <HumidityChart key="humidity-chart" data={weatherData} />
+              </div>
           </div>
 
           <div className="col-12 col-md-12">
-            <WindTrendLine key="wind-chart" data={weatherData} />
+          <div className="dashboard-card">
+              <WindTrendLine key="wind-chart" data={weatherData} />
+              </div>
           </div>
 
           <div className="col-12 col-md-6">
-            <HottestDaysList key="HottestDays-chart" data={weatherData} />
+          <div className="dashboard-card">
+              <HottestDaysList key="HottestDays-chart" data={weatherData} />
+              </div>
           </div>
           <div className="col-12 col-md-6">
-            <TemperatureHeatmap key="TemperatureHeatmap-chart" data={weatherData} />
+          <div className="dashboard-card">
+              <TemperatureHeatmap key="TemperatureHeatmap-chart" data={weatherData} />
+              </div>
           </div>
         </div>
       </div>
