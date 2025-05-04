ac387684dc12a80070d398507402328c7507df21
“KellyReisLee” <kristinaalvares28@gmail.com>
2025-05-04 14:27:30 +0100

    Removing unnecessary divs

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -30,42 +30,42 @@ function Sidebar() {
 
   return (
     <>
-      <div>
-        {/* Botão Hamburguer */}
-        <button
-          className={`hamburger-btn ${showButton ? 'show' : 'hide'}`}
-          onClick={toggleSidebar}
-        >
-          Menu
+
+      {/* Botão Hamburguer */}
+      <button
+        className={`hamburger-btn ${showButton ? 'show' : 'hide'}`}
+        onClick={toggleSidebar}
+      >
+        Menu
+      </button>
+
+      {/* Sidebar */}
+      <div
+        className={`sidebar ${isOpen ? 'open' : ''}`}
+        onTransitionEnd={handleTransitionEnd}
+      >
+        <button onClick={toggleSidebar} className="close-btn">
+          <FaWindowClose className='icon-close' />
         </button>
+        <div className="sidebar-header">
+          <form className="search-form" role="search">
+            <input type="search" placeholder="Buscar..." className="search-input" />
+            <button className="btn-new">Novo</button>
+          </form>
 
-        {/* Sidebar */}
-        <div
-          className={`sidebar ${isOpen ? 'open' : ''}`}
-          onTransitionEnd={handleTransitionEnd}
-        >
-          <button onClick={toggleSidebar} className="close-btn">
-            <FaWindowClose className='icon-close' />
-          </button>
-          <div className="sidebar-header">
-            <form className="search-form" role="search">
-              <input type="search" placeholder="Buscar..." className="search-input" />
-              <button className="btn-new">Novo</button>
-            </form>
-
-          </div>
-
-          <nav className="sidebar-nav">
-            <ul>
-              <li><Link onClick={toggleSidebar} to="/dashboard/tempo-dia"><FaSun /> Tempo do dia</Link></li>
-              <li><Link onClick={toggleSidebar} to="/dashboard/previsoes"><FaCloudSunRain /> Previsões</Link></li>
-              <li><Link onClick={toggleSidebar} to="/dashboard/mapa-tempo"><FaMapMarkedAlt /> Mapa do tempo</Link></li>
-              <li><Link onClick={toggleSidebar} to="/dashboard/estastisticas"><FaChartLine /> Estatísticas</Link></li>
-              <li><Link onClick={toggleSidebar} to="/contacts/3"><FaBars /> Outros dados</Link></li>
-            </ul>
-          </nav>
         </div>
+
+        <nav className="sidebar-nav">
+          <ul>
+            <li><Link onClick={toggleSidebar} to="/dashboard/tempo-dia"><FaSun /> Tempo do dia</Link></li>
+            <li><Link onClick={toggleSidebar} to="/dashboard/previsoes"><FaCloudSunRain /> Previsões</Link></li>
+            <li><Link onClick={toggleSidebar} to="/dashboard/mapa-tempo"><FaMapMarkedAlt /> Mapa do tempo</Link></li>
+            <li><Link onClick={toggleSidebar} to="/dashboard/estastisticas"><FaChartLine /> Estatísticas</Link></li>
+            <li><Link onClick={toggleSidebar} to="/contacts/3"><FaBars /> Outros dados</Link></li>
+          </ul>
+        </nav>
       </div>
+
     </>
   );
 }
