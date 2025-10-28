2c76e2fb8b33ecfe4f4306fcfa3266679b5aa39b
enrique-fcnr <enriquefcnrgta@gmail.com>
2025-10-28 12:21:02 -0300

    Estilização da página Proposito.jsx

--- src/components/Proposito/Proposito.jsx
+++ src/components/Proposito/Proposito.jsx
@@ -4,16 +4,17 @@ import './Proposito.css';
 const Proposito = () => {
   return (
     <main className="proposito-container">
-      <section className="proposito-content">
+      {/* Container Glassmorphism */}
+      <section className="proposito-glass-content">
         <h1 className="proposito-title">Propósito</h1>
         <p className="proposito-paragraph">
           O <strong>Alertaê</strong> é um aplicativo desenvolvido com o objetivo de alertar de maneira amigável e eficaz a população sobre condições climáticas adversas, como chuvas fortes, altas temperaturas e outros problemas ambientais que podem representar riscos à saúde e segurança das pessoas.
         </p>
         <p className="proposito-paragraph">
-          Com uma interface intuitiva e acessível, o app oferece notificações em tempo real sobre eventos climáticos extremos e fornece recomendações de segurança personalizadas com base na localização do usuário. Além disso, o AlertaÊ tem como missão promover a conscientização ambiental, ajudando os cidadãos a tomarem decisões informadas para proteger sua saúde e bem-estar.
+          Com uma interface intuitiva e acessível, o app oferece notificações em tempo real sobre eventos climáticos extremos e fornece recomendações de segurança personalizadas com base na localização do usuário. Além disso, o Alertaê tem como missão promover a conscientização ambiental, ajudando os cidadãos a tomarem decisões informadas para proteger sua saúde e bem-estar.
         </p>
         <p className="proposito-paragraph">
-          Com o AlertaÊ, a informação chega de forma clara e direta, garantindo que todos possam se preparar e agir com antecedência diante de situações climáticas perigosas.
+          Com o Alertaê, a informação chega de forma clara e direta, garantindo que todos possam se preparar e agir com antecedência diante de situações climáticas perigosas.
         </p>
       </section>
     </main>
